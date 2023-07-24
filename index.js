ee0d967b45c5944088e4f5bff3b471432ebb25ec
Rupaprasanna <rupaambati9@gmail.com>
2023-07-24 15:35:39 +0000

    superoverleague

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  <div className="bg_container">
+    <h1 className="heading"> Super Over League </h1>
+    <div className="image_container">
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
